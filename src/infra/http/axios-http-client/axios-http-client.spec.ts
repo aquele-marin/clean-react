ea8c61f0784d9e2fec9f9ceade3d97cac458bf03
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-12-05 22:33:45 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,6 +5,11 @@ import { HttpPostParams } from "@/data/protocols/http"
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosResult = {
+    data: faker.helpers.arrayElement([{ name: faker.person.fullName(), email: faker.internet.email() }, { name: faker.person.fullName(), email: faker.internet.email() }]),
+    status: faker.number.int()
+}
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
 const makeSut = (): AxiosHttpClient => {
     return new AxiosHttpClient()
@@ -21,6 +26,16 @@ describe('AxiosHttpClient', () => {
         const sut = makeSut()
         await sut.post(request)
         expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+    })
+
+    test('Should return the correct statusCode and body', async () => {
+        const request = mockPostRequest()
+        const sut = makeSut()
+        const response = await sut.post(request)
+        expect(response).toEqual({
+            statusCode: mockedAxiosResult.status,
+            body: mockedAxiosResult.data
+        })
     })
 
 })
