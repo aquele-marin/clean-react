b2f7bd696f77fe7503eef53b613a1b280780ecbb
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-12-13 02:29:11 -0300

    feat: ensure MinLengthValidation returns error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,15 @@
+import { InvalidFieldError } from "@/validation/errors"
+import { FieldValidation } from "@/validation/protocols/field-validation"
+import { MinLengthValidation } from "./min-length-validation"
+
+
+
+const makeSut = (): any => new MinLengthValidation('field', 5)
+
+describe('MinLengthValidation', () => {
+    test('Should return error if value is invalid', () => {
+        const sut = makeSut()
+        const error = sut.validate('123')
+        expect(error).toEqual(new InvalidFieldError())
+    })
+})
