acae878c705cea119e8c90a4169afb905f4eea14
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-12-12 23:03:10 -0300

    feat: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -90,5 +90,19 @@ describe('Login Component', () => {
         expect(emailStatus.textContent).toBe('')
     })
 
+    test('Should enable submit button if form is valid', () => {
+        const { sut, validationStub } = makeSut()
+        validationStub.errorMessage = null
+
+        const emailInput = sut.getByTestId('email')
+        fireEvent.input(emailInput, {target: { value: faker.internet.email()}})
+
+        const passwordInput = sut.getByTestId('password')
+        fireEvent.input(passwordInput, {target: { value: faker.internet.password()}})
+
+        const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+        expect(submitButton.disabled).toBe(false)
+    })
+
 
 })
