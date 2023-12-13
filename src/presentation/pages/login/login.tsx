9774ee923ca0efd7258df53545adebcc6b79a4d3
Gabriel Marin <gabrielvbmarin21@gmail.com>
2023-12-13 01:10:50 -0300

    feat: ensure Login navigates to main page on success

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -5,6 +5,7 @@ import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 import { Authentication } from '@/domain/usecases'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {
     validation?: Validation,
@@ -12,6 +13,7 @@ type Props = {
 }
 
 const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
+    const navigate = useNavigate()
     const [state, setState] = useState({
         isLoading: false,
         email: '',
@@ -40,6 +42,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
             })
             const account = await authentication.auth({ email: state.email, password: state.password })
             localStorage.setItem('accessToken', account.accessToken)
+            navigate("/", { replace: true })
         } catch (error) {
             setState({
                 ...state,
